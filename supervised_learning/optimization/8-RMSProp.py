550d8b9bcd4551a0a2681d30aa97c2709e5775e3
IkireziI <i.ikirezi@alustudent.com>
2025-02-04 21:16:51 +0200

    done

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
-""" Training with momentum
-"""
+""" Training with RMSProp Optimization """
 
 import tensorflow as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """ creates the training operation for a neural network in tensorflow
-        using the RMSProp optimization algorithm
-        Args:
-            loss: is the loss of the network
-            alpha: is the learning rate
-            beta2: is the RMSProp weight
-            epsilon: is a small number to avoid division by zero
-        Returns: the RMSProp optimization operation
+    """
+    Creates the training operation for a neural network in TensorFlow
+    using the RMSProp optimization algorithm.
+
+    Args:
+        loss: The loss tensor of the network.
+        alpha: Learning rate.
+        beta2: Decay factor for RMSProp (equivalent to rho in TF2).
+        epsilon: Small value to prevent division by zero.
+
+    Returns:
+        A TensorFlow training operation.
     """
 
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
-    return optimizer.minimize(loss)
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha, rho=beta2, epsilon=epsilon)
+
+    # Compute gradients and apply updates
+    trainable_vars = tf.compat.v1.trainable_variables()
+    gradients = tf.gradients(loss, trainable_vars)
+    train_op = optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+    return train_op
