c4129bb3b9415451f5e5f56b8a14bda4babfdaf4
IkireziI <i.ikirezi@alustudent.com>
2025-02-04 20:38:16 +0200

    fixed the error of file 2-12_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" L2 Regularization Cost
-"""
-
+""" L2 Regularization Cost """
 
 import tensorflow as tf
 
-
 def l2_reg_cost(cost):
-    """ Calculates the cost of a neural network with L2 regularization
+    """ 
+    Calculates the cost of a neural network with L2 regularization.
 
     Args:
-        cost (float): is a tensor containing the cost of the network
-        without L2 regularization
+        cost (tf.Tensor): Tensor containing the cost of the network without L2 regularization.
+
+    Returns:
+        tf.Tensor: Regularized cost including L2 losses.
     """
-    return cost + tf.losses.get_regularization_losses()
+    return cost + tf.reduce_sum(tf.compat.v1.losses.get_regularization_losses())
