130f3fc6489e90d70f43d164f1f620643b460b8a
IkireziI <i.ikirezi@alustudent.com>
2025-06-29 00:08:57 +0200

    push

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -16,7 +16,6 @@ class GaussianProcess:
         """
         A function that initializes a noiseless 1D Gaussian process
         """
-
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -27,7 +26,6 @@ class GaussianProcess:
         """
         calculates the covariance kernel matrix between two matrices
         """
-
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(
             X2**2, 1) - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
@@ -37,7 +35,6 @@ class GaussianProcess:
         predicts mean and standard deviation of points in a
         Gaussian process
         """
-
         K_inv = np.linalg.inv(self.K)
         K_s = self.kernel(self.X, X_s)
         mu = K_s.T.dot(K_inv).dot(self.Y).reshape(-1)
@@ -49,8 +46,6 @@ class GaussianProcess:
         """
         A function that updates a Gaussian process
         """
-
         self.X = np.append(self.X, X_new).reshape(-1, 1)
         self.Y = np.append(self.Y, Y_new).reshape(-1, 1)
         self.K = self.kernel(self.X, self.X)
-        
