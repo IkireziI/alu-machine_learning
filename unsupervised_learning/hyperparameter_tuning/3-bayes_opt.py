fc7985c45a140a571fd03e6447427733f32f053e
IkireziI <i.ikirezi@alustudent.com>
2025-06-29 00:17:50 +0200

    done

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -14,10 +14,22 @@ class BayesianOptimization:
     A class that represents a Bayesian optimization on a
     noiseless 1D Gaussian process
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
-                 sigma_f=1, xsi=0.01, minimize=True):
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         Initializes Bayesian Optimization
+
+        Parameters:
+        - f: the black-box function to be optimized
+        - X_init: numpy.ndarray of shape (t, 1), initial input points
+        - Y_init: numpy.ndarray of shape (t, 1), initial output values
+        - bounds: tuple (min, max), bounds of the search space
+        - ac_samples: number of samples to evaluate acquisition function
+        - l: kernel length parameter
+        - sigma_f: kernel output variance parameter
+        - xsi: exploration-exploitation factor
+        - minimize: boolean indicating whether to minimize or maximize f
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
@@ -27,10 +39,15 @@ class BayesianOptimization:
 
     def acquisition(self):
         """
-        Calculates the next best sample location
+        Calculates the next best sample location using Expected Improvement
+
+        Returns:
+        - X_next: numpy.ndarray of shape (1,), next best sample point
+        - EI: numpy.ndarray of shape (ac_samples,), expected improvement values
         """
         mu, sigma = self.gp.predict(self.X_s)
         sigma = sigma.reshape(-1, 1)
+
         with np.errstate(divide='warn'):
             if self.minimize:
                 mu_sample_opt = np.min(self.gp.Y)
@@ -38,28 +55,49 @@ class BayesianOptimization:
             else:
                 mu_sample_opt = np.max(self.gp.Y)
                 imp = mu - mu_sample_opt - self.xsi
+
             Z = imp / sigma
             ei = imp * self._cdf(Z) + sigma * self._pdf(Z)
             ei[sigma == 0.0] = 0.0
+
         return self.X_s[np.argmax(ei)], ei
 
     def _cdf(self, Z):
         """
-        Computes cumulative distribution function (CDF)
-        for standard normal distribution
+        Computes the cumulative distribution function (CDF)
+        for a standard normal distribution
+
+        Parameters:
+        - Z: numpy.ndarray
+
+        Returns:
+        - numpy.ndarray of the same shape as Z
         """
         return 0.5 * (1 + np.erf(Z / np.sqrt(2)))
 
     def _pdf(self, Z):
         """
         Computes the probability density function (PDF)
-        for standard normal distribution
+        for a standard normal distribution
+
+        Parameters:
+        - Z: numpy.ndarray
+
+        Returns:
+        - numpy.ndarray of the same shape as Z
         """
         return (1 / np.sqrt(2 * np.pi)) * np.exp(-0.5 * Z**2)
 
     def optimize(self, iterations=100):
         """
         Optimizes the black-box function
+
+        Parameters:
+        - iterations: number of iterations to run optimization
+
+        Returns:
+        - X_opt: numpy.ndarray of shape (1,), optimal input point
+        - Y_opt: numpy.ndarray of shape (1,), optimal function value
         """
         X_all = []
         for _ in range(iterations):
@@ -67,9 +105,10 @@ class BayesianOptimization:
             Y_next = self.f(X_next)
             self.gp.update(X_next, Y_next)
             X_all.append(X_next)
+
         if self.minimize:
             idx = np.argmin(self.gp.Y)
         else:
             idx = np.argmax(self.gp.Y)
+
         return self.gp.X[idx], self.gp.Y[idx]
-    
